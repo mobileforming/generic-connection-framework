8340691b8ae97cd54383c805b9f74c0187ace92e
wesley.stjohn@gmail.com <wesley.stjohn@mobileforming.com>
2017-08-29 18:12:23 -0700

    test

--- GCFPlayground.playground/Pages/Intro.xcplaygroundpage/Contents.swift
+++ GCFPlayground.playground/Pages/Intro.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 # General Connection Framework
 
 The General Connection Framework (GCF) is part of mobileforming's modularization effort. GCF will allow you to spend less time writing boilerplate networking code, less time dealing with the nitty gritty, sometimes complex code surrounding authentication and retry logic, and more time making your apps awesome. With every new app, you'll be able to reuse existing GCF code as well as create custom plugins specific to your API needs. Every project utilizing the GCF will have an increased level of confidence in the network code, in addition to other shared benefits.
-
+test
 ## Why Use GCF?
 * Never have to write [URLSession](https://developer.apple.com/documentation/foundation/urlsession) code in your app again (cuts down on network code)
 * No more JSON parsing
