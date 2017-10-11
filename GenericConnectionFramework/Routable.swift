a6129896b2283fabfdd093c9b7264e94074005e6
Bain Kennedy <bain.kennedy@mobileforming.com>
2017-10-10 18:47:49 -0700

    Updated GCF and Routable
    - Routable header -> headers
    - Routable now has defaultTimeout and cachePolicy (defined in default
    extension)
    - GCF now has internal constructURLRequest method.
    - GCF now uses headers in routables

--- GenericConnectionFramework/Routable.swift
+++ GenericConnectionFramework/Routable.swift
@@ -12,9 +12,23 @@ import Foundation
 public protocol Routable {
     var path: String { get }
     var method: HTTPMethod { get }
-    var header: [String : String]? { get }
+    var headers: [String : String]? { get }
     var parameters: [String : String]? { get }
     var body: [String : Any]? { get }
+    
+    // To be overriden...
+    var defaultTimeout: TimeInterval { get }
+    var cachePolicy: URLRequest.CachePolicy { get }
+}
+
+extension Routable {
+    var defaultTimeout: TimeInterval {
+        return 60
+    }
+    
+    var cachePolicy: URLRequest.CachePolicy {
+        return .useProtocolCachePolicy
+    }
 }
 
 public enum HTTPMethod: String {
