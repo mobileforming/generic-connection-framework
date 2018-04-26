2f5e74c24518eb76f33c49504c4f3335ab8f760a
Alan Downs <itsalan@gmail.com>
2018-04-26 10:38:33 -0700

    changed build system

---
+++ GenericConnectionFramework.framework/Headers/GenericConnectionFramework.h
@@ -0,0 +1,19 @@
+//
+//  GenericConnectionFramework.h
+//  GenericConnectionFramework
+//
+//  Created by Bain Kennedy on 7/19/17.
+//  Copyright © 2017 mobileforming LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GenericConnectionFramework.
+FOUNDATION_EXPORT double GenericConnectionFrameworkVersionNumber;
+
+//! Project version string for GenericConnectionFramework.
+FOUNDATION_EXPORT const unsigned char GenericConnectionFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericConnectionFramework/PublicHeader.h>
+
+
